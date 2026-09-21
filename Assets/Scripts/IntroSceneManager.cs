7352c45ef0b18a2315f2cefa5aa02d43e5fdeb37
David Carlsson <davc@kth.se>
2026-09-21 10:35:18 +0200

    Basic fixes to introscript

--- Assets/Scripts/IntroSceneManager.cs
+++ Assets/Scripts/IntroSceneManager.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class IntroSceneManager : MonoBehaviour
 {
+    [SerializeField] private int levelIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
